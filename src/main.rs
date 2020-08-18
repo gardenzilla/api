6c5abd0781fe0d3933ddf07bcc7deab44bdbd56b
Peter <mezeipetister@gmail.com>
2020-08-18 10:11:22 +0200

    port check

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,9 @@
 use actix_web::{web, App, HttpRequest, HttpResponse, HttpServer, Responder};
 use protos;
 use tokio::sync::Mutex;
-use tonic::{transport::Server, Request, Response, Status};
+// use tonic::{transport::Server, Request, Response, Status};
+
+const DEFAULT_PORT: u32 = 7000;
 
 struct AppState {
     client: Mutex<protos::user::user_client::UserClient<tonic::transport::Channel>>,
@@ -31,7 +33,18 @@ async fn hello(client: web::Data<AppState>) -> HttpResponse {
 
 #[actix_rt::main]
 async fn main() -> std::io::Result<()> {
-    let mut client = protos::user::user_client::UserClient::connect("http://[::1]:50051")
+    let port = match std::env::var("API_PORT") {
+        Ok(val) => {
+            let _val = val.parse::<u32>().expect("API_PORT ENV must be u32");
+            println!("Running at port {}", _val);
+            _val
+        }
+        Err(_) => {
+            println!("THERE IS NO API_PORT ENV, so set it to {}", DEFAULT_PORT);
+            DEFAULT_PORT
+        }
+    };
+    let client = protos::user::user_client::UserClient::connect("http://[::1]:50051")
         .await
         .unwrap();
 
@@ -44,7 +57,7 @@ async fn main() -> std::io::Result<()> {
             .route("/hello", web::get().to(hello))
             .route("/{name}", web::get().to(greet))
     })
-    .bind("127.0.0.1:7000")?
+    .bind(format!("127.0.0.1:{}", port))?
     .run()
     .await
 }
